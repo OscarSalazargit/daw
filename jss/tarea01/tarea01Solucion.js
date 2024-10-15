1c404cf00c41ea3103ad5f9755e255099527866d
Oscar <uncaminante.es@gmail.com>
2024-10-15 17:00:00 +0200

    colocación en el centro del canvas el logo

--- jss/tarea01/tarea01Solucion.js
+++ jss/tarea01/tarea01Solucion.js
@@ -1,12 +1,16 @@
-const canva = document.getElementById('canvas') ;
-const barco = document.getElementById('barco');
-const camion = document.getElementById('camion');
-const nave = document.getElementById('nave');
-canva.innerHTML="<i class='fa-brands fa-space-awesome js01__logo-i'>";
+const canva = document.getElementById("canvas");
+const barco = document.getElementById("barco");
+const camion = document.getElementById("camion");
+const nave = document.getElementById("nave");
+canva.innerHTML =
+  "<img src='../../assets/img/logoOscar.jpg' alt='Logo de Oscar Salazar' class='header__imagen' />";
 
-barco.addEventListener("click", e =>{
+barco.addEventListener("click", (e) => {
   canva.innerHTML = "<i class='fa-solid fa-ship js01__logo-i'></i>";
-})
-camion.addEventListener("click", e =>{
+});
+camion.addEventListener("click", (e) => {
   canva.innerHTML = '<i class="fa-solid fa-truck js01__logo-i"></i>';
-})
+});
+nave.addEventListener("click", (e) => {
+  canva.innerHTML = '<i class="fa-brands fa-space-awesome js01__logo-i"></i>';
+});
