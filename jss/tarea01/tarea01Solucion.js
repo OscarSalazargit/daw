f7d27af42d219aff3426a2f8649c88ba44db845c
Oscar <uncaminante.es@gmail.com>
2024-10-15 16:51:46 +0200

    Actualización de jss tarea01

--- jss/tarea01/tarea01Solucion.js
+++ jss/tarea01/tarea01Solucion.js
@@ -1,42 +1,12 @@
-// let losLis = document.getElementById("pez");
-// losLis.style.color = "red";
-// losLis.textContent = "Verde";
-// console.log(losLis);
+const canva = document.getElementById('canvas') ;
+const barco = document.getElementById('barco');
+const camion = document.getElementById('camion');
+const nave = document.getElementById('nave');
+canva.innerHTML="<i class='fa-brands fa-space-awesome js01__logo-i'>";
 
-// let lista = document.querySelectorAll("li");
-// console.log(lista);
-
-// lista.forEach((element) => {
-//   element.style.fontSize = "2rem";
-// });
-
-// const inner = document.getElementById("inner");
-// inner.innerHTML = "Hola a <b>Todos!!</b>";
-// inner.style.backgroundColor = "red";
-
-// const outer = document.getElementById("outer");
-// console.log(outer);
-// outer.outerHTML = "<ul><li>A</li><li>B</li></ul>";
-
-// const text = document.getElementById("text");
-// text.innerText = "<b>Adios amigos!!</b>";
-// text.title = "Esto es un titulo informativo!";
-// inner.id = "loco";
-
-// const loco = document.getElementById("loco");
-// loco.textContent = "Telita";
-// console.log(loco);
-// loco.hidden = true;
-
-const imgA = document.getElementsByClassName("a");
-console.log(imgA);
-
-for (im of imgA) {
-  im.style.border = "5px solid blue";
-}
-
-let trans = Array.from(imgA);
-console.log(trans);
-trans.forEach((tr) => {
-  tr.style.border = "9px solid pink";
-});
+barco.addEventListener("click", e =>{
+  canva.innerHTML = "<i class='fa-solid fa-ship js01__logo-i'></i>";
+})
+camion.addEventListener("click", e =>{
+  canva.innerHTML = '<i class="fa-solid fa-truck js01__logo-i"></i>';
+})
